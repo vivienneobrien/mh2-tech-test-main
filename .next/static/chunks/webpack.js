f41abe5c2a90699e17a3d4adc1b7fe5d97b07b3a
vivienneobrien <vxo910@cs.bham.ac.uk>
2021-08-10 12:48:14 +0100

    Changed useEffect() curly braces and changed if statement

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "82252afb9ecaccb01a1b";
+/******/ 	var hotCurrentHash = "c249e793423c3fda190a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
